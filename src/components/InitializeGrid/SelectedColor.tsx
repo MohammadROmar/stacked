78fb18da2497f5553e52e00fe6ff76de6a7df292
MohammadROmar <149619529+MohammadROmar@users.noreply.github.com>
2024-11-06 12:11:32 +0300

    Fixed more images problems

--- src/components/InitializeGrid/SelectedColor.tsx
+++ src/components/InitializeGrid/SelectedColor.tsx
@@ -1,3 +1,4 @@
+import { cellsImages } from '../../assets/images/colors';
 import { getImageName } from '../../utils/get-image-name';
 import type { Symbol } from '../../types/game';
 
@@ -10,7 +11,7 @@ export default function SelectedColor({
     <section className="section p-2 w-auto absolute top-4 left-4 z-10 flex items-center gap-2">
       <p className="w-min text-xs text-center">Selected Color</p>
       <img
-        src={`/src/assets/images/colors/${getImageName(selectedColor)}.png`}
+        src={cellsImages[getImageName(selectedColor)]}
         className="w-8 h-8 shadow-md rounded-xl"
       />
     </section>
